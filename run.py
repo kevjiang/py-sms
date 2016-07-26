674506ad2dcc2b926e691f838d47f822e5f7d604
Kevin Jiang <k.jiang@yale.edu>
2016-07-25 22:48:28 -0400

    adding cookie functionality testing

--- run.py
+++ run.py
@@ -1,19 +1,28 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
 import os
 from handler import *
 
+# The session object makes use of a secret key.
+SECRET_KEY = 'a secret key'
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def receive_sms():
+    counter = session.get('counter', 0)
+    counter += 1
+    session['counter'] = counter
+
     from_number = request.values.get('From', None)
     body = request.values.get('Body', None)
     body = body.lower().strip()  # removes spaces and converts all to lower case
 
     message = response_handler(body)
+    message += " Counter = %s" % str(counter)
  
     resp = twilio.twiml.Response()
+
     resp.message(message)
 
     return str(resp)
